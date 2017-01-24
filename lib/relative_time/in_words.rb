ab25f3582353fbd241753247d3b83e639d6093a5
Anton Davydov <antondavydov.o@gmail.com>
2017-01-25 01:17:42 +0300

    Create relative time string for minutes

--- lib/relative_time/in_words.rb
+++ lib/relative_time/in_words.rb
@@ -2,11 +2,47 @@ module RelativeTime
   class InWords
     def call(time_to)
       diff = Time.now - time_to
-      if diff > 0
-        'less than a minute'
+
+      return 'less than a minute' if diff.abs < 59
+
+      if diff >= 0
+        relative_ago(verb_agreement(resolution(diff)))
       else
-        'more than a minute'
+        relative_later(verb_agreement(resolution(diff.abs)))
       end
+    end
+
+  private
+    def resolution(diff)
+      if diff > 29030400
+        [(diff/29030400).round, 'years']
+      elsif diff > 2419200
+        [(diff/2419200).round, 'months']
+      elsif diff > 604800
+        [(diff/604800).round, 'weeks']
+      elsif diff > 86400
+        [(diff/86400).round, 'days']
+      elsif diff > 3600
+        [(diff/3600).round, 'hours']
+      else
+        [(diff/60).round, 'minutes']
+      end
+    end
+
+    def verb_agreement(resolution)
+      if resolution[0] == 1
+        "a #{resolution.last[0...-1]}"
+      else
+        resolution.join(' ')
+      end
+    end
+
+    def relative_ago(string)
+      "#{string} ago"
+    end
+
+    def relative_later(string)
+      "in #{string}"
     end
   end
 end
--- test/relative_time_test.rb
+++ test/relative_time_test.rb
@@ -5,7 +5,15 @@ describe RelativeTime do
     describe 'when differense in seconds' do
       it { RelativeTime.in_words(Time.now).must_equal      'less than a minute' }
       it { RelativeTime.in_words(Time.now - 10).must_equal 'less than a minute' }
-      it { RelativeTime.in_words(Time.now + 10).must_equal 'more than a minute' }
+      it { RelativeTime.in_words(Time.now + 10).must_equal 'less than a minute' }
+    end
+
+    describe 'when differense in minutes' do
+      it { RelativeTime.in_words(Time.now - 1 * 60).must_equal 'a minute ago' }
+      it { RelativeTime.in_words(Time.now - 3 * 60).must_equal '3 minutes ago' }
+
+      it { RelativeTime.in_words(Time.now + 1 * 60).must_equal 'in a minute' }
+      it { RelativeTime.in_words(Time.now + 3 * 60).must_equal 'in 3 minutes' }
     end
   end
 end
