71d3183750f6a09cae8bc157932f76fea91f237f
Anton Davydov <antondavydov.o@gmail.com>
2017-01-25 01:54:51 +0300

    Add other tests

--- test/relative_time_test.rb
+++ test/relative_time_test.rb
@@ -4,6 +4,9 @@ describe RelativeTime do
   let(:minute) { 60 }
   let(:hour)   { 60 * minute }
   let(:day)    { 24 * hour }
+  let(:week)   { 7  * day }
+  let(:month)  { 4  * week }
+  let(:year)   { 12 * month }
 
   describe '#in_words' do
     describe 'when differense in seconds' do
@@ -60,6 +63,48 @@ describe RelativeTime do
       it { RelativeTime.in_words(Time.now + 1 * day).must_equal 'in a day' }
       it { RelativeTime.in_words(Time.now + 3 * day).must_equal 'in 3 days' }
       it { RelativeTime.in_words(Time.now + 6 * day).must_equal 'in 6 days' }
+    end
+
+    describe 'when differense in weeks' do
+      it { RelativeTime.in_words(Time.now - 7 * day).must_equal  'a week ago' }
+      it { RelativeTime.in_words(Time.now + 7 * day).must_equal  'in a week' }
+
+      it { RelativeTime.in_words(Time.now - 8 * day).must_equal  'a week ago' }
+      it { RelativeTime.in_words(Time.now + 8 * day).must_equal  'in a week' }
+
+      it { RelativeTime.in_words(Time.now - 1 * week).must_equal 'a week ago' }
+      it { RelativeTime.in_words(Time.now - 3 * week).must_equal '3 weeks ago' }
+
+      it { RelativeTime.in_words(Time.now + 1 * week).must_equal 'in a week' }
+      it { RelativeTime.in_words(Time.now + 3 * week).must_equal 'in 3 weeks' }
+    end
+
+    describe 'when differense in months' do
+      it { RelativeTime.in_words(Time.now - 4 * week).must_equal 'a month ago' }
+      it { RelativeTime.in_words(Time.now + 4 * week).must_equal 'in a month' }
+
+      it { RelativeTime.in_words(Time.now - 6 * week).must_equal 'a month ago' }
+      it { RelativeTime.in_words(Time.now + 6 * week).must_equal 'in a month' }
+
+      it { RelativeTime.in_words(Time.now - 1  * month).must_equal 'a month ago' }
+      it { RelativeTime.in_words(Time.now - 11 * month).must_equal '11 months ago' }
+
+      it { RelativeTime.in_words(Time.now + 1  * month).must_equal 'in a month' }
+      it { RelativeTime.in_words(Time.now + 11 * month).must_equal 'in 11 months' }
+    end
+
+    describe 'when differense in years' do
+      it { RelativeTime.in_words(Time.now - 12 * month).must_equal 'a year ago' }
+      it { RelativeTime.in_words(Time.now + 12 * month).must_equal 'in a year' }
+
+      it { RelativeTime.in_words(Time.now - 14 * month).must_equal 'a year ago' }
+      it { RelativeTime.in_words(Time.now + 14 * month).must_equal 'in a year' }
+
+      it { RelativeTime.in_words(Time.now - 1  * year).must_equal 'a year ago' }
+      it { RelativeTime.in_words(Time.now - 11 * year).must_equal '11 years ago' }
+
+      it { RelativeTime.in_words(Time.now + 1  * year).must_equal 'in a year' }
+      it { RelativeTime.in_words(Time.now + 11 * year).must_equal 'in 11 years' }
     end
   end
 end
