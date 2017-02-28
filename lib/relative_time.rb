55cb49fb5fa63a1acbe854414fcd223251d2f117
Anton Davydov <don.mist@gmail.com>
2017-02-28 04:17:36 +0300

    Delete unnecessary line of code

--- lib/relative_time.rb
+++ lib/relative_time.rb
@@ -2,7 +2,6 @@ require 'relative_time/in_words'
 require 'relative_time/version'
 
 module RelativeTime
-  # Your code goes here...
   def self.in_words(date_to, date_from = Time.now)
     InWords.new.(date_to, date_from)
   end
